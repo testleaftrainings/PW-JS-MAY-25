5a14f4ef855f71f4fae8f5bff6e540a40696a2a2
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-05-24 20:58:24 +0530

    Add files via upload

---
+++ Day1/01-get-started.js
@@ -0,0 +1,30 @@
+
+
+//Get started Comment a single line
+
+/* Get 
+Started *//*  --> *//*  Shortcut to commant a paragraph -->Ctrl+Shift+a */
+
+
+// To declare a string --> "Ravindran", '' , ``
+
+
+/* var population =131414141424124124124;
+console.log(population); */
+
+console.log(typeof population); // "bigInt"
+
+
+//string
+
+var username = "demosalesmanager"
+var pwd = '@Testleaf'
+
+console.log(username);
+console.log(pwd);
+
+//typeof operator
+
+console.log(`The datatype of username is `+ typeof username);
+console.log(`The datatype of username is `+ typeof pwd);
+
---
+++ Day1/03-reassignment.js
@@ -0,0 +1,24 @@
+//Using var you can declare and reassign
+
+/* var username = "crmsfa" // declaration
+var username = "usernamecrmsfa" // redeclaration
+var username = "usernamecrmsfa1" // reassignment
+
+console.log(username); */
+
+//Using let you cannot redeclare and but you can reassign
+
+let accountBalance = 10000// declaration
+
+console.log(`$username`+`accountBalance`);
+
+accountBalance="60000"// reassignment 
+console.log(`${accountBalance}`);
+
+//Using const  you cannot redeclare and but you can reassign
+
+const accountNumber = 1231231231// declaration
+//const accountNumber=60000// redeclaration 
+accountNumber=12412412// reassignment
+
+console.log(accountNumber);
